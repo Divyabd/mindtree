19dfcf6bed49b6d50d5a7dd79da1db3eeef53580
Divyabd <divyagowda8393@gmail.com>
2021-02-08 10:21:38 +0530

    1stt

---
+++ Sample/src/sample/Sample.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is divya");
+		System.out.println("This is divya");
+		
+	}
+
+}
